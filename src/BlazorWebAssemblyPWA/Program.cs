d44aff0a4d2a1d5d88fc692ba990a52aaf49e69e
Jboy Flaga <flaga.jeremiah@gmail.com>
2020-11-05 09:18:46 +0800

    re-organized folders

--- src/BlazorWebAssemblyPWA/Program.cs
+++ src/BlazorWebAssemblyPWA/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace eShopOnWeb.BlazorWebAssemblyPWA
+namespace BlazorWebAssemblyPWA
 {
 	public class Program
 	{
--- tests/UnitTests/CounterTests.cs
+++ tests/UnitTests/CounterTests.cs
@@ -1,19 +1,25 @@
 using Bunit;
-using eShopOnWeb.BlazorWebAssemblyPWA.Pages;
+using BlazorWebAssemblyPWA.Pages;
 using System;
 using Xunit;
 
-namespace eShopOnWeb.BlazorWebAssemblyPWA.UnitTests
+namespace UnitTests
 {
 	public class CounterTests
 	{
         // References: 
         // Test components in ASP.NET Core Blazor: https://docs.microsoft.com/en-us/aspnet/core/blazor/test?view=aspnetcore-3.1
         // Creating a new bUnit Test Project: https://bunit.egilhansen.com/docs/getting-started/create-test-project.html?tabs=xunit
+        // Writing Tests in C# for Blazor Components: https://bunit.egilhansen.com/docs/getting-started/writing-csharp-tests.html?tabs=xunit
         [Fact]
         public void CounterShouldIncrementWhenSelected()
         {
             // Arrange
+            // Use bUnit to render the component under test, pass in its parameters, inject required services, 
+            // and access the rendered component instance and the markup it has produced.
+            // Rendering a component happens through bUnit's TestContext. The result of the rendering 
+            // - a IRenderedComponent<TComponent> - provides access to the component instance and the markup 
+            // produced by the component.
             using var ctx = new TestContext();
             var cut = ctx.RenderComponent<Counter>();
             var paraElm = cut.Find("p");
